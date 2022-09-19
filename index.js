2bf45cfbace421c334c9e03172982688a024a391
Gustavo <gstavomota1@gmail.com>
2022-09-18 21:26:15 -0300

    rodando api

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("server rodando");
+const app = require('./api/config/expressConfig');
+require('dotenv').config();
+
+const port = process.env.PORT;
+
+app.listen(port, console.log(`servidor rodando na porta ${port}`));
