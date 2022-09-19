54da840bbc8d303dbd3b6178d49926a86a225e30
Gustavo <gstavomota1@gmail.com>
2022-09-19 00:16:40 -0300

    criadas as rotas get post put e delete

--- api/config/expressConfig.js
+++ api/config/expressConfig.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-
 app.use(express.urlencoded({
     extended:true
 }));
+
+const musicaRouter = require('../src/domains/musicas/controllers/index');
+app.use('/api/musica', musicaRouter);
 
 module.exports = app;
--- api/src/domains/musicas/controllers/index.js
+++ api/src/domains/musicas/controllers/index.js
@@ -2,3 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Musica = require('../models/Musica');
 
+router.get('/', (req,res) =>{
+    res.status(200).send(Musica);
+})
+
+router.post('/',(req,res) =>{
+    console.log(req.body);
+    Musica.push(req.body);
+
+    res.status(200).send(req.body);
+});
+
+router.put('/:nome',(req,res)=>{
+    var nome = req.params.nome;
+    //const achaMusica = Musica.find(Musica=>Musica.nome === nome);
+    const achaMusica = Musica.findIndex(Musica=>Musica.nome === nome);
+
+    console.log(achaMusica);
+    Musica[achaMusica].nome = req.body.nome;
+    Musica[achaMusica].artista = req.body.artista;
+    Musica[achaMusica].genero = req.body.genero;
+    Musica[achaMusica].quantidadeDownloads = req.body.quantidadeDownloads;
+
+    res.status(200).send(Musica[achaMusica]);
+});
+
+router.delete('/:nome',(req,res)=>{
+    var nomeDel = req.params.nome;
+
+    const achaMusicaDel = Musica.findIndex(Musica => Musica.nome === nomeDel);
+    
+    Musica.splice(achaMusicaDel,1);
+
+    res.status(200).send(Musica);
+})
+
+
+module.exports = router;
